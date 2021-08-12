3c480ddbd6794180d07c8cf53c50ab0d6646583b
Henrique Elias Silva <henriqueeliassilva@gmail.com>
2021-08-11 22:40:20 -0300

    landing page

--- pages/landing.js
+++ pages/landing.js
@@ -6,23 +6,23 @@ function Type () {
                             </h1>
                             <p className=" text-center mb-5 text-lg md:text-2xl text-white">Cadastre-se para mais informações.</p>
                         </div>
-                        <div className="bg-gray-800 p-8 rounded">
-                                    <form  method="POST" action="https://henrique94039.activehosted.com/proc.php" id="_form_1_"  noValidate> 
-                                            <input type="hidden" name="u" value="1" />
-                                            <input type="hidden" name="f" value="1" />
-                                            <input type="hidden" name="s" />
-                                            <input type="hidden" name="c" value="0" />
-                                            <input type="hidden" name="m" value="0" />
-                                            <input type="hidden" name="act" value="sub" />
-                                            <input type="hidden" name="v" value="2" />
-                                            
-                                            <input className="mb-2 w-full border border-gray-200 rounded" type="text" id="fullname" name="fullname" placeholder="Digite seu nome" />
-                                            <input className="mb-2 w-full border border-gray-200 rounded" type="text" id="email" name="email" placeholder="Digite seu e-mail" required/>
-                                                <button className="w-full text-center rounded py-2  bg-green-500 text-white hover:bg-green-600 focus:outline-none" id="_form_1_submit" type="submit">
-                                                    Enviar
-                                                </button>
-                                    </form>
-                     </div>
+                                <div className="bg-gray-800 p-8 rounded">
+                                                <form  method="POST" action="https://henrique94039.activehosted.com/proc.php" id="_form_1_"  noValidate> 
+                                                        <input type="hidden" name="u" value="1" />
+                                                        <input type="hidden" name="f" value="1" />
+                                                        <input type="hidden" name="s" />
+                                                        <input type="hidden" name="c" value="0" />
+                                                        <input type="hidden" name="m" value="0" />
+                                                        <input type="hidden" name="act" value="sub" />
+                                                        <input type="hidden" name="v" value="2" />
+                                                        
+                                                        <input className="mb-2 w-full border border-gray-200 rounded" type="text" id="fullname" name="fullname" placeholder="Digite seu nome" />
+                                                        <input className="mb-2 w-full border border-gray-200 rounded" type="text" id="email" name="email" placeholder="Digite seu e-mail" required/>
+                                                            <button className="w-full text-center rounded py-2  bg-green-500 text-white hover:bg-green-600 focus:outline-none" id="_form_1_submit" type="submit">
+                                                                Enviar
+                                                            </button>
+                                                </form>
+                                </div>
              </div>  
               
          </>
